263a0ae7245a54bab423065cafc546a03ff8d8aa
Dominique Dietiker <dominique.dietiker@momoland.ch>
2017-03-06 07:59:55 +0100

    DDAuthentication as independent module
    - The old AuthenticationModule has been renamed DDAuthenticationModule
    - dd-login (from DDLoginComponent) is a component with selector that
    can be integrated in any page (portability)
    - Alert Component has been renamed DDAlertComponent and included in a
    DDAlertModule. You can use dd-alert everywhere in the application, as
    long as you include it.

--- src/app/science/science.routing.ts
+++ src/app/science/science.routing.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AuthGuard } from '../authentication/index';
+import { DDAuthGuard } from '../dd-authentication/index';
 import { ScienceHomeComponent } from './index';
 
 const scienceRoutes: Routes = [
