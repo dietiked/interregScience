96b81fa82348505a728f09fa4ef5c9ee94071c8c
Dominique Dietiker <dominique.dietiker@momoland.ch>
2017-03-09 09:02:46 +0100

    Edit form (only header)
    It's now possible to navigate to an edit page (that solves #3), but only
    header is loaded.

--- src/app/science/science.routing.ts
+++ src/app/science/science.routing.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { DDAuthGuard } from '../dd-authentication/index';
 import { ScienceHomeComponent } from './index';
-import { FormNewComponent } from './index';
+import { FormNewComponent, FormEditComponent } from './index';
 
 const scienceRoutes: Routes = [
     { path: 'science', component: ScienceHomeComponent, canActivate: [DDAuthGuard], children: [
     ]}, // },
     { path: 'science/forms/new/:formCategoryId', component: FormNewComponent, canActivate: [DDAuthGuard]},
+    { path: 'science/forms/:id', component: FormEditComponent, canActivate: [DDAuthGuard]},
     // otherwise redirect to home
 ];
 
