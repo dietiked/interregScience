bdf0327cd71f5eead968e22c06d9ac959a895255
Dominique Dietiker <dominique.dietiker@momoland.ch>
2017-07-04 08:04:42 +0200

    Save

--- src/app/science/_services/pest.service.ts
+++ src/app/science/_services/pest.service.ts
@@ -59,35 +59,4 @@ export class PestService {
       pestRef.update(pest);
     }
   }
-
-  // If valuesForFormId --> get form values for pests
-  /*public getPestsForFormCategoryWithKey(formDefinitionId:string, valuesForFormId?:string) {
-    // Discover, which pests are available for this form definition
-    let pests = this.database.list(ScienceConstants.listPestsForDefinitionWithKey(formDefinitionId))
-    .map((pests:Pest[]) => {
-      // Iterate through the form pests
-      for (let pest of pests) {
-        // Get pest definition
-        this.database.object(ScienceConstants.objectPestWithKey(pest.$key))
-        .subscribe(pestDefinition => {
-          var formPest = new Pest(pestDefinition);
-          pest.name = pestDefinition.name;
-          if (valuesForFormId) {
-            this.getValuesForPestInForm(formPest.$key, valuesForFormId)
-            .subscribe(values => {
-              pest.control = values.control;
-              pest.v1 = values.v1;
-              pest.v2 = values.v2;
-              pest.v3 = values.v3;
-              pest.v4 = values.v4;
-              pest.v5 = values.v5;
-            });
-          }
-        });
-      }
-      return pests;
-    })
-    return pests;
-  }
-  */
 }
